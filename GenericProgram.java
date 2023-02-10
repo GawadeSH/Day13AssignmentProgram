0ef18604efcb9118396c30e6b1c999b780c51956
shital gawade <shitalgawade131@gmail.com>
2023-02-10 15:14:22 +0530

    Float number

--- GenericProgram.java
+++ GenericProgram.java
@@ -8,30 +8,48 @@ public class GenericProgram
     {
 
         Integer[] a1 = {12, 7, 8};
+        Float[] a2 = {1.2f, 7.4f, 8.1f};
         GenericProgram.testMax(a1);
+        GenericProgram.testMaxFloat(a2);
+
     }
-   static void testMax(Integer[] a1)
-        {
-            if (a1[0].compareTo(a1[1]) == 1)
+   static void testMax(Integer[] a1) {
+       if (a1[0].compareTo(a1[1]) == 1) {
+           if (a1[0].compareTo(a1[2]) == 1) {
+               System.out.println("maximum number is :- " + a1[0]);
+           } else if (a1[0].compareTo(a1[2]) == -1) {
+               System.out.println("maximum number is :- " + a1[2]);
+           }
+       } else if (a1[0].compareTo(a1[1]) == -1) {
+           if (a1[1].compareTo(a1[2]) == 1) {
+               System.out.println("maximum number is :- " + a1[1]);
+           } else if (a1[1].compareTo(a1[2]) == -1) {
+               System.out.println("maximum number is :- " + a1[2]);
+           }
+       }
+   }
+            static void testMaxFloat(Float[] a2)
             {
-                if (a1[0].compareTo(a1[2]) == 1)
+                if (a2[0].compareTo(a2[1]) == 1)
                 {
-                    System.out.println("maximum number is :- " + a1[0]);
-                } else if (a1[0].compareTo(a1[2]) == -1) {
-                    System.out.println("maximum number is :- " + a1[2]);
+                    if (a2[0].compareTo(a2[2]) == 1)
+                    {
+                        System.out.println("maximum number is :- " + a2[0]);
+                    } else if (a2[0].compareTo(a2[2]) == -1) {
+                        System.out.println("maximum number is :- " + a2[2]);
+                    }
                 }
-            }
-            else if (a1[0].compareTo(a1[1]) == -1)
-            {
-                if (a1[1].compareTo(a1[2]) == 1)
+                else if (a2[0].compareTo(a2[1]) == -1)
                 {
-                    System.out.println("maximum number is :- " + a1[1]);
+                    if (a2[1].compareTo(a2[2]) == 1)
+                    {
+                        System.out.println("maximum number is :- " + a2[1]);
+                    }
+                    else if (a2[1].compareTo(a2[2]) == -1)
+                    {
+                        System.out.println("maximum number is :- " + a2[2]);
+                    }
                 }
-                else if (a1[1].compareTo(a1[2]) == -1)
-                {
-                    System.out.println("maximum number is :- " + a1[2]);
-                }
-            }
         }
     }
 
