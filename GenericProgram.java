8a999c47468d650156f68b198ecdd98f5d9ca855
shital gawade <shitalgawade131@gmail.com>
2023-02-10 15:29:05 +0530

    Sting valuer

--- GenericProgram.java
+++ GenericProgram.java
@@ -9,21 +9,33 @@ public class GenericProgram
 
         Integer[] a1 = {12, 7, 8};
         Float[] a2 = {1.2f, 7.4f, 8.1f};
+        String[] a3 = {"Shital","Pratiksha","Greeshma"};
         GenericProgram.testMax(a1);
         GenericProgram.testMaxFloat(a2);
+        GenericProgram.testMaxString(a3);
 
     }
-   static void testMax(Integer[] a1) {
-       if (a1[0].compareTo(a1[1]) == 1) {
-           if (a1[0].compareTo(a1[2]) == 1) {
+   static void testMax(Integer[] a1)
+   {
+       if (a1[0].compareTo(a1[1]) == 1)
+       {
+           if (a1[0].compareTo(a1[2]) == 1)
+           {
                System.out.println("maximum number is :- " + a1[0]);
-           } else if (a1[0].compareTo(a1[2]) == -1) {
+           }
+           else if (a1[0].compareTo(a1[2]) == -1)
+           {
                System.out.println("maximum number is :- " + a1[2]);
            }
-       } else if (a1[0].compareTo(a1[1]) == -1) {
-           if (a1[1].compareTo(a1[2]) == 1) {
+       }
+       else if (a1[0].compareTo(a1[1]) == -1)
+       {
+           if (a1[1].compareTo(a1[2]) == 1)
+           {
                System.out.println("maximum number is :- " + a1[1]);
-           } else if (a1[1].compareTo(a1[2]) == -1) {
+           }
+           else if (a1[1].compareTo(a1[2]) == -1)
+           {
                System.out.println("maximum number is :- " + a1[2]);
            }
        }
@@ -35,7 +47,9 @@ public class GenericProgram
                     if (a2[0].compareTo(a2[2]) == 1)
                     {
                         System.out.println("maximum number is :- " + a2[0]);
-                    } else if (a2[0].compareTo(a2[2]) == -1) {
+                    }
+                    else if (a2[0].compareTo(a2[2]) == -1)
+                    {
                         System.out.println("maximum number is :- " + a2[2]);
                     }
                 }
@@ -50,6 +64,31 @@ public class GenericProgram
                         System.out.println("maximum number is :- " + a2[2]);
                     }
                 }
+            }
+                static void testMaxString(String[] a3)
+                {
+                    if (a3[0].compareTo(a3[1]) > 0)
+                    {
+                        if (a3[0].compareTo(a3[2]) > 0)
+                        {
+                            System.out.println("String is :- " + a3[0]);
+                        }
+                        else if (a3[0].compareTo(a3[2]) < 0)
+                        {
+                            System.out.println("string is  :- " + a3[2]);
+                        }
+                    }
+                    else if (a3[0].compareTo(a3[1]) < 0)
+                    {
+                        if (a3[1].compareTo(a3[2]) > 0)
+                        {
+                            System.out.println("String is :- " + a3[1]);
+                        }
+                        else if (a3[1].compareTo(a3[2]) < 0)
+                        {
+                            System.out.println("String is:- " + a3[2]);
+                        }
+                    }
         }
     }
 
